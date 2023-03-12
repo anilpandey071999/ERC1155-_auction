1753b9f918caa2c317fe5ad83e2bd653a8828cf5
anil.p6 <31802461+anilpandey071999@users.noreply.github.com>
2023-03-12 16:40:25 +0530

    add erc20 token pyment

--- contracts/Auction.sol
+++ contracts/Auction.sol
@@ -1,9 +1,14 @@
-// SPDX-License-Identifier= MIT
-pragma solidity ^0.8.17;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "hardhat/console.sol";
 
-contract Auction is ERC1155 {
+contract Auction is ERC1155, Ownable {
+    IERC20 token;
     /**
      * Bidder keeps the records of bidder details
      * bidder is the address of wallet placing bid
@@ -33,21 +38,35 @@ contract Auction is ERC1155 {
         bool auctionOpen;
         uint256 startingTime;
         uint256 auctionDuration;
-        mapping(uint32 => Bidder) biddings;
+        uint256 totalBalance;
     }
+    mapping(uint256 => mapping(uint32 => Bidder)) biddings;
+    mapping(address => bool) isBidded;
 
     mapping(uint256 => AuctionIteam) public auctions;
 
-    uint256 auctionId = 1;
-    uint256 tokenId = 1;
+    uint256 public auctionId = 1;
+    uint256 public tokenId = 1;
+    uint32 public maxBid = 10;
 
-    constructor()ERC1155("https://game.example/api/item/{id}.json") {}
+    constructor(IERC20 _token) ERC1155("") {
+        token = _token;
+    }
 
-    function mint(address account, uint256 amount) public {
-        _mint(account, tokenId, amount, "");
+    /**
+     * @param amount is total supply of token id as it ERC1155 is the combile of ERC20 and ERC721
+     */
+    function mint(uint256 amount) public {
+        _mint(msg.sender, tokenId, amount, "");
         tokenId = tokenId + 1;
     }
 
+    /**
+     * @param id is a token id
+     * @param numberOfToken number of token will be auctioned of the same token 1d
+     * @param auctionDuration is an auction duration
+     * @param minmumAmount an minmum amount of bid
+     */
     function createAuction(
         uint256 id,
         uint256 numberOfToken,
@@ -61,19 +80,163 @@ contract Auction is ERC1155 {
         auctionIteam.tokenId = id;
         auctionIteam.numberOfToken = numberOfToken;
         auctionIteam.minmumPrice = minmumAmount;
-        auctionIteam.totalBid = 0;
+        auctionIteam.totalBid = 1;
         auctionIteam.auctionOpen = true;
         auctionIteam.startingTime = block.timestamp;
         auctionIteam.auctionDuration = auctionDuration;
 
+        auctionIteam.totalBalance = 0;
+
         auctionId = auctionId + 1;
     }
 
-    /**
-     * register the 1155 token
-     * create an auction for the token
-     * Place bidding in auction
-     * place
-     *
-     */
+    function newBid(uint256 _auctionId, uint amount) public  {
+        AuctionIteam storage auctionIteam = auctions[_auctionId];
+        require(!isBidded[msg.sender], "You have already placed you bid");
+        require(
+            amount >= auctionIteam.minmumPrice,
+            "amount should not be less then minmum value"
+        );
+        uint256 auctionEndTime = auctionIteam.startingTime +
+            auctionIteam.auctionDuration;
+        require(auctionEndTime > block.timestamp, "Auction Ended");
+        if (auctionIteam.totalBid > maxBid) {
+            (
+                uint256 lowestBid,
+                address lowestBidder,
+                uint32 lowestBidderIndex
+            ) = findLowestBider(_auctionId);
+            auctionIteam.totalBalance = auctionIteam.totalBalance - lowestBid;
+           Withdraw(lowestBidder ,lowestBid);
+           deposit(msg.sender,amount);
+            biddings[_auctionId][lowestBidderIndex] = Bidder(
+                msg.sender,
+                amount
+            );
+
+        } else {
+            deposit(msg.sender,amount);
+            biddings[_auctionId][auctionIteam.totalBid] = Bidder(
+                msg.sender,
+                amount
+            );
+        }
+        auctionIteam.totalBalance = auctionIteam.totalBalance + amount;
+        auctionIteam.totalBid = auctionIteam.totalBid + 1;
+        isBidded[msg.sender] = true;
+    }
+
+
+    function findLowestBider(
+        uint256 _auctionId
+    )
+        internal
+        view
+        returns (
+            uint256 lowestBid,
+            address lowestBidder,
+            uint32 lowestBidderIndex
+        )
+    {
+        // AuctionIteam storage auctionIteam = auctions[auctionId];
+        uint256 _lowestBid = biddings[_auctionId][1].bidAmount;
+        address _lowestBidder = biddings[_auctionId][1].bidder;
+        uint32 _lowestBidderIndex = 1;
+        for (uint32 i = 1; i <= maxBid; i++) {
+            if (_lowestBid > biddings[_auctionId][i].bidAmount) {
+                _lowestBid = biddings[_auctionId][i].bidAmount;
+                _lowestBidder = biddings[_auctionId][i].bidder;
+                _lowestBidderIndex = i;
+            }
+        }
+        return (_lowestBid, _lowestBidder, _lowestBidderIndex);
+    }
+
+    function findHightestBider(
+        uint256 _auctionId
+    )
+        internal
+        view
+        returns (
+            uint256 hightestBid,
+            address hightestBidder,
+            uint32 hightestBidderIndex
+        )
+    {
+        // AuctionIteam storage auctionIteam = auctions[auctionId];
+        uint256 _hightestBid = biddings[_auctionId][1].bidAmount;
+        address _hightestBidder = biddings[_auctionId][1].bidder;
+        uint32 _hightestBidderIndex = 1;
+        for (uint32 i = 1; i <= maxBid; i++) {
+            if (_hightestBid > biddings[_auctionId][i].bidAmount) {
+                _hightestBid = biddings[_auctionId][i].bidAmount;
+                _hightestBidder = biddings[_auctionId][i].bidder;
+                _hightestBidderIndex = i;
+            }
+        }
+        return (_hightestBid, _hightestBidder, _hightestBidderIndex);
+    }
+
+    function endAuction(uint256 _auctionId) public {
+        AuctionIteam storage auctionIteam = auctions[_auctionId];
+        require(
+            auctionIteam.artist == msg.sender,
+            "Only artist of the auction can call this function"
+        );
+        (uint256 hightestBid, address hightestBidder, ) = findHightestBider(
+            _auctionId
+        );
+        IERC1155(address(this)).safeTransferFrom(
+            msg.sender,
+            hightestBidder,
+            auctionIteam.tokenId,
+            auctionIteam.numberOfToken,
+            ""
+        );
+
+        auctionIteam.auctionOpen = false;
+
+        for (uint32 i = 1; i <= maxBid; i++) {
+            if (biddings[_auctionId][i].bidder != hightestBidder) {
+                Withdraw(
+                    biddings[_auctionId][i].bidder,
+                    biddings[_auctionId][i].bidAmount
+                );
+            } else {
+                Withdraw(auctionIteam.artist, hightestBid);
+            }
+        }
+    }
+
+    function getMyBid(
+        uint256 _auctionId
+    ) external view returns (uint256 myBid) {
+        // AuctionIteam storage auctionIteam = auctions[_auctionId];
+        for (uint32 i = 1; i <= maxBid; i++) {
+            if (msg.sender == biddings[_auctionId][i].bidder) {
+                myBid = biddings[_auctionId][i].bidAmount;
+            }
+        }
+        // console.log(biddings[_auctionId][1].bidAmount, index,msg.sender == biddings[_auctionId][1].bidder);
+        return myBid;
+    }
+
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+
+    function changeMaxBid(uint32 _maxBid) external onlyOwner {
+        maxBid = _maxBid;
+    }
+
+    mapping(address => uint256) public userbalance;
+
+    function deposit(address sender, uint256 amount) internal {
+        token.transferFrom(sender, address(this), amount);
+        userbalance[sender] = amount;
+    }
+
+    function Withdraw(address sender,uint256 amount) internal {
+        token.transfer(sender, amount);
+    }
 }
--- test/Auction.ts
+++ test/Auction.ts
@@ -2,21 +2,30 @@ import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
 import { ethers } from "hardhat";
-import { Auction, Auction__factory } from "../typechain-types"
+import { Auction, Auction__factory, MyToken, MyToken__factory } from "../typechain-types"
 
 describe("Auction", function () {
     async function deployAuction() {
         const [user1, user2, user3] = await ethers.getSigners();
+        const ERC20Token: MyToken__factory = await ethers.getContractFactory("MyToken")
+        const erc20Token: MyToken = await ERC20Token.deploy();
         const AuctionContract: Auction__factory = await ethers.getContractFactory("Auction");
-        const auctionContract: Auction = await AuctionContract.deploy();
+        const auctionContract: Auction = await AuctionContract.deploy(erc20Token.address);
 
-        return { auctionContract, user1, user2, user3 }
+        await erc20Token.connect(user1).mint(ethers.utils.parseEther("150"))
+        await erc20Token.connect(user1).approve(auctionContract.address,ethers.utils.parseEther("150"))
+        await erc20Token.connect(user2).mint(ethers.utils.parseEther("150"))
+        await erc20Token.connect(user2).approve(auctionContract.address,ethers.utils.parseEther("150"))
+        await erc20Token.connect(user3).mint(ethers.utils.parseEther("150"))
+        await erc20Token.connect(user3).approve(auctionContract.address,ethers.utils.parseEther("150"))
+
+        return { auctionContract, erc20Token, user1, user2, user3 }
     }
 
     describe("Auction", function () {
         it("Mint ERC1155", async function () {
-            const { auctionContract, user1, user2, user3 } = await deployAuction();
-            await auctionContract.mint(user1.address, 1)
+            const { auctionContract, erc20Token, user1, user2, user3 } = await deployAuction();
+            await auctionContract.mint(1)
 
             expect(await (await auctionContract.balanceOf(user1.address, 1)).toString()).to.be.equal("1")
         })
@@ -24,17 +33,87 @@ describe("Auction", function () {
         it("Create Auction", async function () {
             const { auctionContract, user1, user2, user3 } = await deployAuction();
             const tokenId = 1;
-            await auctionContract.mint(user1.address, 1)
+            await auctionContract.mint(1)
 
             expect(await (await auctionContract.balanceOf(user1.address, tokenId)).toString()).to.be.equal("1")
             const ONE_HOURS_IN_SECS = 1 * 60 * 60;
-            await auctionContract.createAuction(tokenId, 1, (await time.latest()) + ONE_HOURS_IN_SECS, ethers.utils.parseEther("0.15"));
+            await auctionContract.createAuction(tokenId, 1, ONE_HOURS_IN_SECS, ethers.utils.parseEther("0.15"));
             
             // console.log(user1.address,(await (await auctionContract.auctions(1)).artist) );
             
             expect(await (await (await auctionContract.auctions(1)).artist)).to.be.equal(user1.address)
         })
 
+        it("Place Bid", async function () {
+            const { auctionContract,erc20Token, user1, user2, user3 } = await deployAuction();
+            await auctionContract.mint(1)
+            const tokenId = 1;
+
+            const ONE_HOURS_IN_SECS = 1 * 60 * 60;
+            const creatAuction =  await auctionContract.connect(user1).createAuction(tokenId, 1, (await time.latest()) + ONE_HOURS_IN_SECS, ethers.utils.parseEther("0.15"));
+            await creatAuction.wait();
+
+            
+            const placebid = await auctionContract.connect(user2).newBid(1,ethers.utils.parseEther("0.16"))
+            await placebid.wait()
+            
+            expect(await (await auctionContract.connect(user2).getMyBid(1)).toString()).to.be.equal(ethers.utils.parseEther("0.16").toString())
+        })
+
+        it("Placeing MAX Bid", async function () {
+            const { auctionContract,erc20Token, user1, user2, user3 } = await deployAuction();
+            const [,,,user4, user5, user6, user7, user8, user9, user10, user11] = await ethers.getSigners()
+            await erc20Token.connect(user4).mint(ethers.utils.parseEther("150"))
+            await erc20Token.connect(user4).approve(auctionContract.address,ethers.utils.parseEther("150"))
+            await erc20Token.connect(user5).mint(ethers.utils.parseEther("150"))
+            await erc20Token.connect(user5).approve(auctionContract.address,ethers.utils.parseEther("150"))
+            await erc20Token.connect(user6).mint(ethers.utils.parseEther("150"))
+            await erc20Token.connect(user6).approve(auctionContract.address,ethers.utils.parseEther("150"))
+            await erc20Token.connect(user7).mint(ethers.utils.parseEther("150"))
+            await erc20Token.connect(user7).approve(auctionContract.address,ethers.utils.parseEther("150"))
+            await erc20Token.connect(user8).mint(ethers.utils.parseEther("150"))
+            await erc20Token.connect(user8).approve(auctionContract.address,ethers.utils.parseEther("150"))
+            await erc20Token.connect(user9).mint(ethers.utils.parseEther("150"))
+            await erc20Token.connect(user9).approve(auctionContract.address,ethers.utils.parseEther("150"))
+            await erc20Token.connect(user10).mint(ethers.utils.parseEther("150"))
+            await erc20Token.connect(user10).approve(auctionContract.address,ethers.utils.parseEther("150"))
+            await erc20Token.connect(user11).mint(ethers.utils.parseEther("150"))
+            await erc20Token.connect(user11).approve(auctionContract.address,ethers.utils.parseEther("150"))
+            await auctionContract.mint(1)
+            const tokenId = 1;
+
+            const ONE_HOURS_IN_SECS = 1 * 60 * 60;
+           const creatAuction =  await auctionContract.connect(user1).createAuction(tokenId, 1, (await time.latest()) + ONE_HOURS_IN_SECS, ethers.utils.parseEther("0.15"));
+            await creatAuction.wait();
+            const user1Placebid = await auctionContract.connect(user1).newBid(1, ethers.utils.parseEther("1"))
+            await user1Placebid.wait()
+            const user2Placebid = await auctionContract.connect(user2).newBid(1,ethers.utils.parseEther("0.17"))
+            await user2Placebid.wait()
+            const user2Balance = parseInt(await (await erc20Token.balanceOf(user2.address)).toString())
+            const usder3placebid = await auctionContract.connect(user3).newBid(1,ethers.utils.parseEther("0.18"))
+            await usder3placebid.wait()
+            const usder4placebid = await auctionContract.connect(user4).newBid(1,ethers.utils.parseEther("0.19"))
+            await usder4placebid.wait()
+            const usder5placebid = await auctionContract.connect(user5).newBid(1,ethers.utils.parseEther("0.20"))
+            await usder5placebid.wait()
+            const usder6placebid = await auctionContract.connect(user6).newBid(1,ethers.utils.parseEther("0.21"))
+            await usder6placebid.wait()
+            const usder7placebid = await auctionContract.connect(user7).newBid(1,ethers.utils.parseEther("0.22"))
+            await usder7placebid.wait()
+            const usder8placebid = await auctionContract.connect(user8).newBid(1,ethers.utils.parseEther("0.23"))
+            await usder8placebid.wait()
+            const usder9placebid = await auctionContract.connect(user9).newBid(1,ethers.utils.parseEther("0.24"))
+            await usder9placebid.wait()
+            const usder10placebid = await auctionContract.connect(user10).newBid(1,ethers.utils.parseEther("0.25"))
+            await usder10placebid.wait()
+            const usder11placebid = await auctionContract.connect(user11).newBid(1,ethers.utils.parseEther("0.26"))
+            await usder11placebid.wait()
+            const user2BalanceAfter = parseInt(await (await erc20Token.balanceOf(user2.address)).toString())
+            
+            expect(user2BalanceAfter).to.be.greaterThan(user2Balance)
+
+            // expect(await (await auctionContract.connect(user2).getMyBid(1)).toString()).to.be.equal(ethers.utils.parseEther("0.15").toString())
+        })
 
     })
 })
